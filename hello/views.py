d5680b6afd91742f81cb64a4cbcf6e1a2817ca88
preethanugu <preethamanugu@gmail.com>
2023-08-27 19:32:17 -0500

    incremental changes

--- hello/views.py
+++ hello/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, "hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
